5e4d1099d933527d1ab8095dafde8f3778b60485
CelsoTJr <ctaddeojunior@yahoo.com.br>
2024-10-07 09:46:57 -0300

    classificar

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("Teste push/pull")
-console.log("aeee deu tudo certo")
+// 1 Classificador de Nível de Herói
+// Variaveis - Operadores - Laços de repetição - Estruturas de decisão
+//Objetivo - Crie uma variável para armazenar o nome e a quantidade de XP de um herói,
+//depois utilize uma estrutura de decisão p/ apresentar alguma das msgs abaixo:
+//se XP for: 
+//menor que 1000 = Ferro
+//entre 1001 e 2000 = Bronze
+//entre 2001 e 5000 = Prata
+//entre 5001 e 7000 = Ouro
+//entre 7001 e 8000 = Platina
+//entre 8001 e 9000 = Ascendente
+//entre 9001 e 10000 = Imortal
+//maior que 10000 = Radiante
+
+nome = "Jhonny Cage"
+qtdXp = 0
+
